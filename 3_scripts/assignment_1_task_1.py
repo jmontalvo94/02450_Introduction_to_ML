0b4f0e7bfe2c0299dfe7a979e1bc3f3c823e8bd1
emilcphu <emil.chrisander@econ.ku.dk>
2019-09-17 11:08:07 +0200

    added to assignemt 1 script

--- 3_scripts/assignment_1_task_1.py
+++ 3_scripts/assignment_1_task_1.py
@@ -2,6 +2,21 @@
 """
 Created on Tue Sep 17 10:05:50 2019
 
-@author: pdj165
+@author: Emil Chrisander
 """
 
+import pandas as pd
+import numpy as np
+
+## Set URL for raw data
+url_raw_data = "https://drive.google.com/uc?export=download&id=1DEmiVdf5UGOo8lqNvNiiHqJQuiNCgxc2"
+#url_raw_data = "http://www-stat.stanford.edu/~tibs/ElemStatLearn/datasets/SAheart.data"
+
+
+## Load raw data file (South African Heart Disease dataset)
+df_heart_disease = pd.read_csv(url_raw_data)
+
+## Initial data manipulations
+df_heart_disease = df_heart_disease.drop(columns = "row.names", axis = 1) ## erases column 'row.names'. Axis = 1 indicates it is a column rather than row drop.
+
+df_heart_disease.head()
